15b99b722d3dbba3a959f17d600f8819f568ac52
HSQMG <9981hoang@gmail.com>
2025-11-01 21:40:11 +0700

    a

--- src/app/edit-products/page.tsx
+++ src/app/edit-products/page.tsx
@@ -2,14 +2,12 @@ import ProductEditor from "../components/ProductEditor";
 // import HeroEditor from "../components/HeroEditor";
 import FeaturedCategoriesEditor from "../components/FeaturedCategoriesEditor";
 import FashionSectionEditor from "../components/FashionSectionEditor";
-import ProductHighlightEditor from "../components/ProductHighlightEditor";
 export default function EditProductsPage() {
   return (
     <main className="min-h-screen bg-gray-100">
       <FeaturedCategoriesEditor />
       <ProductEditor />
       <FashionSectionEditor />
-      <ProductHighlightEditor />
     </main>
   );
 }
